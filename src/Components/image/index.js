05e4ce152990d328559e9e9c90802943e5a4c9f4
abdurrehman2002 <abdurrehman.ptf786@gmail.com>
2022-12-10 06:56:18 -0500

    fixed eslint issues

--- src/Components/image/index.js
+++ src/Components/image/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Image(props) {
   return (
     <div>
-      <img  style={style.image} width={props.width} {...props} src={props.image}  />
+      <img  style={style.image} width={props.width} {...props} src={props.image} alt={props.alt}  />
     </div>
   )
 }
